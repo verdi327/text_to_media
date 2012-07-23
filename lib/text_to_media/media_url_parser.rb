f0b72ebc22600779a54bc4c3f35721eb1d5816db
michael verdi <michael.v.verdi@gmail.com>
2012-07-23 10:41:33 -0400

    added display_html method

--- lib/text_to_media/media_url_parser.rb
+++ lib/text_to_media/media_url_parser.rb
@@ -1,7 +1,7 @@
 class MediaUrlParser
   EMBEDLY_KEY = "9e226384957f11e1bf614040aae4d8c9"
   URL_REGEX = /http[s]?:\/\/\S+\.(?:com|net|org|biz|in|co|info|gov|ly|me|im)(?:\?.*)?\S{1,}/i
-  attr_accessor :original_string, :html, :url, :has_url
+  attr_accessor :original_string, :html, :url, :has_url, :display_html
 
   def initialize(original_string)
     self.original_string = original_string
@@ -13,13 +13,20 @@ class MediaUrlParser
 
   def check_for_media
     if contains_url
-      self.has_url  = true
-      self.html     = parse(embedly_response)
+      self.has_url      = true
+      self.html         = parse(embedly_response)
+      self.display_html = format_with_original_message
     else
-      self.has_url  = false
-      self.html     = "<p>#{original_string}</p>"
+      self.has_url      = false
+      self.html         = "<p>#{original_string}</p>"
+      self.display_html = "<p>#{original_string}</p>"
     end
     self
+  end
+
+  def format_with_original_message
+    html_style = "</br>#{self.html}</br>"
+    self.original_string.gsub(URL_REGEX, html_style)
   end
 
   def embedly_response
